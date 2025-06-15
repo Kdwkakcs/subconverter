131defa047c944d33ddae5a7384914e8d3511c8f
Kdwkakcs <morchidblondi831@gmail.com>
2025-06-15 08:45:19 +0000

    add

--- scripts/build.ubuntu.library.sh
+++ scripts/build.ubuntu.library.sh
@@ -1,17 +1,47 @@
 #!/bin/bash
 set -xe
 
+# 默认不安装g++
+INSTALL_GPP=false
+
+# 解析命令行参数
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --install-gpp)
+            INSTALL_GPP=true
+            shift
+            ;;
+        -h|--help)
+            echo "用法: $0 [选项]"
+            echo "选项:"
+            echo "  --install-gpp    安装g++-13编译器"
+            echo "  -h, --help       显示此帮助信息"
+            exit 0
+            ;;
+        *)
+            echo "未知参数: $1"
+            echo "使用 -h 或 --help 查看帮助信息"
+            exit 1
+            ;;
+    esac
+done
+
 # 安装必要的工具和库
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt update
+# 如果需要安装g++
+if [ "$INSTALL_GPP" = true ]; then
+    echo "安装g++-13编译器..."
+    add-apt-repository ppa:ubuntu-toolchain-r/test -y
+    apt update
+    apt install g++-13 -y
+    update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
+    update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
+else
+    echo "跳过g++安装 (使用 --install-gpp 参数来安装)"
+fi
 
-sudo apt install g++-13 -y
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-13 100
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-13 100
-
-sudo apt update
-sudo apt install gcc g++ build-essential cmake make autoconf automake libtool python3 -y
-sudo apt install libmbedtls-dev zlib1g-dev rapidjson-dev libpcre2-dev -y
+apt update
+apt install gcc g++ build-essential cmake make autoconf automake libtool python3 -y
+apt install libmbedtls-dev zlib1g-dev rapidjson-dev libpcre2-dev -y
 
 # 构建并安装静态版本的 curl
 git clone https://github.com/curl/curl --depth=1 --branch curl-8_4_0
